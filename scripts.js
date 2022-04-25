3f3ff18ccf0c68894eedf32970f6d0a2f1f54be4
Aaron <aaronlo826@gmail.com>
2022-04-25 19:10:22 -0400

    added some media queries and started on hamburger dropdown menu

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// // hamburger menu
+
+// document.getElementById('hamburger-icon').addEventListener('click', hamburgerMenu)
+
+// const hamburgerMenu = () => {
+//   const
+//   document.querySelector('.hamburger-nav').classList.add()
+// }
