b5242bb085b6c5d9bd9b447f683b27f0c158175b
Aaron <aaronlo826@gmail.com>
2022-04-25 22:39:58 -0400

    fixed hamburger menu from showing after resizing to larger window

--- scripts.js
+++ scripts.js
@@ -1,17 +1,32 @@
 // hamburger menu
 
 const hamburgerDropdown = document.querySelector('#hamburger-icon')
+
 const hamburgerNav = document.querySelector('.hamburger-nav')
+
+const navButton = document.querySelectorAll('a.hamburger-anchor')
 
 hamburgerDropdown.addEventListener('click', function() {
 
-  if (hamburgerNav.style.opacity === '1'){
-    // hamburgerNav.style.display = "none";
-    hamburgerNav.style.opacity = "0";
-  } else {
-    // hamburgerNav.style.display = "block";
-    hamburgerNav.style.opacity = "1";
+  if (hamburgerNav.style.display === 'none'){
+    hamburgerNav.style.display = "flex";
+    hamburgerDropdown.innerHTML = `<i class="fa-solid fa-xmark fa-2x"></i>`;
+  } else if (hamburgerNav.style.display = 'flex'){
+    hamburgerNav.style.display = "none";
+    hamburgerDropdown.innerHTML = `<i class="fa-solid fa-bars fa-2x">`;
   }
-
 })
 
+// navButton.addEventListener('click', function(){
+  // hamburgerNav.style.display = "none";
+  // hamburgerDropdown.innerHTML = `<i class="fa-solid fa-bars fa-2x">`;
+// })
+
+navButton.forEach((link) => {
+  link.addEventListener('click', function(){
+    hamburgerNav.style.display = "none";
+    hamburgerDropdown.innerHTML = `<i class="fa-solid fa-bars fa-2x">`;
+  })
+})
+
+console.log(navButton);
